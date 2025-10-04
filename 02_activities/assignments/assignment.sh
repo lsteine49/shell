891c6e14a4a1d3de1eac73b46ee78b6eaf362a0e
Liane <lianesteiner93@gmail.com>
2025-10-03 20:56:40 -0400

    git commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,3 +1,19 @@
+# open proper directory
+cd /c/Users/liane/Dropbox/liane/PhD/"Data science course"/"DSI Assignment 1"
+
+# clone git repo
+git clone https://github.com/lsteine49/shell.git
+
+# open shell folder
+cd shell
+
+# create branch for work 
+git switch -c assignment
+
+# open assignmnet 
+cd 02_activities/assignments
+code assignment.sh
+
 #!/bin/bash
 set -x
 
@@ -28,22 +44,32 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+mkdir raw
+mv rawdata raw
 # 3. List the contents of the ./data/raw directory
-
+ls raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-
+mkdir processed
+cd processed
+mkdir server_logs user_logs  event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cp raw/rawdata/server* processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-
+cp raw/rawdata/user* processed/user_logs
+cp raw/rawdata/event* processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm raw/rawdata/*ipaddr*
+rm processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
-
+touch inventory.txt
+find processed -type f > inventory.txt
+cat inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
+
+#commit changes
+git add assignment.sh
+git commit
