6fe0299a2648a70ed5a2495b735b4e83df012076
Liane <lianesteiner93@gmail.com>
2025-10-07 20:14:57 -0400

    test

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,7 +39,7 @@ mkdir processed
 cd processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd data
+cd ..
 cp ./raw/*server*.log ./processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 cp ./raw/*user*.log ./processed/user_logs/
